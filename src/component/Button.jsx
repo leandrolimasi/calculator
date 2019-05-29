276be61939779ba319ad1444461408068c2be47a
Leandro Silva <leandro_silva@gap.com>
2019-05-29 11:48:23 -0300

    adding operations events

--- src/component/Button.jsx
+++ src/component/Button.jsx
@@ -4,8 +4,7 @@ import './Button.css';
 export default props => 
     <button 
     onClick={e=> props.click && props.click(props.label)}
-    className={`button ${props.operation} ? 'operation': '' ${props.double} ? 'double': ''  ${props.triple} ? 'triple': ''  `}
-    
+        className={`button ${props.operation ? 'operation' : ''} ${props.double ? 'double' : ''}  ${props.triple ? 'triple' : ''}  `}
     >
         {props.label} 
     </button>
